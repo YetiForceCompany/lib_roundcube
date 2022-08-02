a30bcddf963a217e07ddff2c7eefaf3d4210b3e4
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2022-08-02 13:31:20 +0200

    Custom plugins update: authres_status, contextmenu, identity_smtp, thunderbird_labels

--- plugins/thunderbird_labels/config.inc.php
+++ plugins/thunderbird_labels/config.inc.php
@@ -1,14 +1,14 @@
 <?php
-// whether to globally enable thunderbird labels
-$rcmail_config['tb_label_enable'] = true;
-// add labels to contextmenu (if contextmenu plugin is present)
-$rcmail_config['tb_label_enable_contextmenu'] = true;
-// enable kb shortcuts (1-5)
-$rcmail_config['tb_label_enable_shortcuts'] = true;
-// users can modify labels
-$rcmail_config['tb_label_modify_labels'] = true;
-// style for UI: 'bullets' or 'thunderbird'
-$rcmail_config['tb_label_style'] = "bullets";
-// custom hidden flags
-$rcmail_config['tb_label_hidden_flags'] = array();
+  // whether to globally enable thunderbird labels
+  $config['tb_label_enable'] = true;
+  // add labels to contextmenu (if contextmenu plugin is present)
+  $config['tb_label_enable_contextmenu'] = true;
+  // enable kb shortcuts (1-5)
+  $config['tb_label_enable_shortcuts'] = true;
+  // users can modify labels
+  $config['tb_label_modify_labels'] = true;
+  // style for UI: 'bullets' or 'thunderbird'
+  $config['tb_label_style'] = "bullets";
+  // custom hidden flags
+  $config['tb_label_hidden_flags'] = array();
 
