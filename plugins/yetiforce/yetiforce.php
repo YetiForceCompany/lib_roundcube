2afe26896bfd13f23526027a98ac1a44f349fb3d
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2022-11-12 21:22:37 +0100

    Add support autologin from YetiForceCRM

--- plugins/yetiforce/yetiforce.php
+++ plugins/yetiforce/yetiforce.php
@@ -217,6 +217,7 @@ class yetiforce extends rcube_plugin
 		if (empty($_SESSION['user_id']) && !empty($_GET['_autologin'])) {
 			$args['action'] = 'login';
 		}
+
 		return $args;
 	}
 
@@ -230,28 +231,188 @@ class yetiforce extends rcube_plugin
 	public function authenticate($args): array
 	{
 		if (!empty($_GET['_autologin']) && ($row = $this->getAutoLogin())) {
-			$host = false;
-			foreach ($this->rc->config->get('imap_host') as $key => $value) {
-				if (false !== strpos($key, $row['mail_host'])) {
-					$host = $key;
-				}
-			}
-			if ($host) {
-				$currentPath = getcwd();
-				chdir($this->rc->config->get('root_directory'));
-				require_once 'include/main/WebUI.php';
-				$args['user'] = $row['username'];
-				$args['pass'] = \App\Encryption::getInstance()->decrypt($row['password']);
-				$args['host'] = $host;
-				$args['cookiecheck'] = false;
-				$args['valid'] = true;
-				chdir($currentPath);
-			}
 			$db = $this->rc->get_dbh();
 			$db->query('DELETE FROM `u_yf_mail_autologin` WHERE `cuid` = ?;', $row['cuid']);
+
+			$currentPath = getcwd();
+			chdir($this->rc->config->get('root_directory'));
+			require_once 'include/main/WebUI.php';
+			if (\App\Record::isExists($row['ruid'], 'MailAccount')) {
+				$mailAccount = \App\Mail\Account::getInstanceById($row['ruid']);
+				if ($mailAccount->isActive()) {
+					$imap = $mailAccount->getServer()->getImapHost();
+					$smtp = $mailAccount->getServer()->getSmtpHost();
+					$this->rc->config->set('imap_host', [$imap => $imap]);
+					$this->rc->config->set('smtp_host', [$smtp => $smtp]);
+					$args['user'] = $mailAccount->getLogin();
+					$args['pass'] = $mailAccount->getPassword();
+					$args['host'] = $imap;
+					$args['cookiecheck'] = false;
+					$args['valid'] = true;
+
+					$_SESSION['smtp_host'] = $smtp;
+					$_SESSION['crm']['id'] = $args['cuid'];
+					$this->add_hook('smtp_connect', [$this, 'smtp_connect']);
+					if ($mailAccount->getServer()->isOAuth()) {
+						$token = $mailAccount->getAccessToken();
+						$args['pass'] = $oauthToken = "Bearer {$token}";
+						$oauthData = [
+							'token_type' => 'Bearer',
+							'expires_in' => \App\Fields\DateTime::getDiff(date('Y-m-d H:i:s'), $mailAccount->getExpireTime(), 'seconds'),
+							'scope' => implode(' ', $mailAccount->getOAuthProvider()->getScopesByAction('MailAccount')),
+							'expires' => strtotime($mailAccount->getExpireTime()) - 10,
+							'refresh_token' => $this->rc->encrypt($mailAccount->getRefreshToken()),
+							'mailAccountId' => $mailAccount->getSource()->getId()
+						];
+						$this->rc->config->set('imap_auth_type', 'XOAUTH2');
+						$this->rc->config->set('login_password_maxlen', \strlen($oauthToken));
+
+						$_SESSION['oauth_token'] = $oauthData;
+						$this->add_hook('managesieve_connect', [$this, 'managesieve_connect']);
+						$this->add_hook('refresh', [$this, 'refresh']);
+					}
+				}
+			}
+			chdir($currentPath);
 		}
 		return $args;
 	}
+
+	// START OAuth
+
+	/**
+	 * Callback for 'storage_init' hook.
+	 *
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	public function storage_init($options)
+	{
+		if (isset($_SESSION['oauth_token']) && 'imap' === $options['driver']) {
+			if ($this->check_token_validity($_SESSION['oauth_token'])) {
+				$options['password'] = $this->rcmail->decrypt($_SESSION['password']);
+			}
+			$options['auth_type'] = 'XOAUTH2';
+		}
+		if (isset($options['fetch_headers'])) {
+			$options['fetch_headers'] = trim($options['fetch_headers'] . ' RECEIVED'); // ??
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Check the given access token data if still valid.
+	 *
+	 * ... and attempt to refresh if possible.
+	 *
+	 * @param array $token
+	 *
+	 * @return bool
+	 */
+	protected function check_token_validity($token)
+	{
+		if ($token['expires'] < time() && !empty($token['refresh_token']) && !empty($token['mailAccountId'])) {
+			return false !== $this->refresh_access_token($token);
+		}
+		return false;
+	}
+
+	/**
+	 * Obtain a new access token using the refresh_token grant type.
+	 *
+	 * If successful, this will update the `oauth_token` entry in
+	 * session data.
+	 *
+	 * @param array $token
+	 *
+	 * @return array Updated authorization data
+	 */
+	public function refresh_access_token(array $token)
+	{
+		$response = false;
+		$mailAccoountId = (int) $token['mailAccountId'];
+		$currentPath = getcwd();
+		chdir($this->rc->config->get('root_directory'));
+		require_once 'include/main/WebUI.php';
+		if (\App\Record::isExists($mailAccoountId, 'MailAccount')) {
+			try {
+				$mailAccount = \App\Mail\Account::getInstanceById($mailAccoountId);
+				$token = $mailAccount->getAccessToken();
+				$oauthToken = "Bearer {$token}";
+				$_SESSION['password'] = $this->rc->encrypt($oauthToken);
+				$token['expires'] = strtotime($mailAccount->getExpireTime()) - 10;
+				$_SESSION['oauth_token'] = $token;
+				$response = [
+					'token' => $token,
+					'authorization' => $oauthToken,
+				];
+			} catch (\Throwable $e) {
+				$response = false;
+			}
+		}
+		chdir($currentPath);
+
+		return $response;
+	}
+
+	/**
+	 * Callback for 'smtp_connect' hook.
+	 *
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	public function smtp_connect($options)
+	{
+		if (isset($_SESSION['smtp_host'])) {
+			$options['smtp_host'] = $_SESSION['smtp_host'];
+		}
+		if (isset($_SESSION['oauth_token'])) {
+			$this->check_token_validity($_SESSION['oauth_token']);
+			// enforce XOAUTH2 authorization type
+			$options['smtp_user'] = $_SESSION['username'] ?? '%u';
+			$options['smtp_pass'] = isset($_SESSION['password']) ? $this->rc->decrypt($_SESSION['password']) : '%p';
+			$options['smtp_auth_type'] = 'XOAUTH2';
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Callback for 'managesieve_connect' hook.
+	 *
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	public function managesieve_connect($options)
+	{
+		if (isset($_SESSION['oauth_token'])) {
+			$this->check_token_validity($_SESSION['oauth_token']);
+			// enforce XOAUTH2 authorization type
+			$options['auth_type'] = 'XOAUTH2';
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Callback for 'refresh' hook.
+	 *
+	 * @param array $options
+	 *
+	 * @return void
+	 */
+	public function refresh($options)
+	{
+		if (isset($_SESSION['oauth_token'])) {
+			$this->check_token_validity($_SESSION['oauth_token']);
+		}
+	}
+
+	// END OAUTH
 
 	/**
 	 * login_after hook handler.
@@ -278,6 +439,10 @@ class yetiforce extends rcube_plugin
 			$args['_composeKey'] = App\Purifier::purifyByType(rcube_utils::get_input_string('_composeKey', rcube_utils::INPUT_GET), 'Alnum');
 		}
 		if ($row = $this->getAutoLogin()) {
+			$sql = 'UPDATE ' . $this->rc->db->table_name('users') . ' SET crm_ma_id = ? WHERE user_id = ?';
+			\call_user_func_array([$this->rc->db, 'query'], array_merge([$sql], [$row['ruid'], $this->rc->get_user_id()]));
+			$this->rc->db->affected_rows();
+
 			$_SESSION['crm']['id'] = $row['cuid'];
 			if (!empty($row['params']['language'])) {
 				$language = $row['params']['language'];
@@ -319,7 +484,7 @@ class yetiforce extends rcube_plugin
 		}
 		$key = App\Purifier::purifyByType(rcube_utils::get_input_string('_autologinKey', rcube_utils::INPUT_GPC), 'Alnum');
 		$db = $this->rc->get_dbh();
-		$sqlResult = $db->query('SELECT * FROM u_yf_mail_autologin INNER JOIN roundcube_users ON roundcube_users.user_id = u_yf_mail_autologin.ruid WHERE roundcube_users.password <> \'\' AND u_yf_mail_autologin.`key` = ?;', $key);
+		$sqlResult = $db->query('SELECT * FROM u_yf_mail_autologin WHERE u_yf_mail_autologin.`key` = ?;', $key);
 		$autologin = false;
 		if ($row = $db->fetch_assoc($sqlResult)) {
 			$autologin = $row;
@@ -615,9 +780,9 @@ class yetiforce extends rcube_plugin
 	{
 		$currentPath = getcwd();
 		chdir($this->rc->config->get('root_directory'));
-		$config = Settings_Mail_Config_Model::getConfig('signature');
+		$signature = \App\Mail::getConfig('signature', 'signature');
 		$parser = App\TextParser::getInstanceById($this->currentUser->getId(), 'Users');
-		$result = $parser->setContent($config['signature'])->parse()->getContent();
+		$result = $parser->setContent($signature)->parse()->getContent();
 		chdir($currentPath);
 		return ['text' => $result, 'html' => $result];
 	}
@@ -631,9 +796,9 @@ class yetiforce extends rcube_plugin
 	{
 		$currentPath = getcwd();
 		chdir($this->rc->config->get('root_directory'));
-		$config = Settings_Mail_Config_Model::getConfig('signature');
+		$signatureActive = \App\Mail::getConfig('signature', 'addSignature');
 		chdir($currentPath);
-		return empty($config['addSignature']) || 'false' === $config['addSignature'] ? true : false;
+		return empty($signatureActive);
 	}
 
 	/**
@@ -1084,18 +1249,6 @@ class yetiforce extends rcube_plugin
 				$this->rc->output->command('addSenderToListMove', $junkMbox);
 			}
 		}
-	}
-
-	/**
-	 * storage_init hook handler.
-	 * Adds additional headers to supported headers list.
-	 *
-	 * @param array $p
-	 */
-	public function storage_init(array $p): array
-	{
-		$p['fetch_headers'] = trim(($p['fetch_headers'] ?? '') . ' RECEIVED');
-		return $p;
 	}
 
 	/**
