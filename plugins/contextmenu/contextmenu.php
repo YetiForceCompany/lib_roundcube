bb6c4542559dd5f4a972af3a6baa9f79e9554741
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2026-02-10 12:50:05 +0100

    plugins

--- plugins/contextmenu/contextmenu.php
+++ plugins/contextmenu/contextmenu.php
@@ -31,6 +31,7 @@ class contextmenu extends rcube_plugin
     public $task = '?(?!login$|logout$|cli$).*';
     private $rcube;
 
+    #[\Override]
     public function init()
     {
         $this->rcube = rcmail::get_instance();
@@ -87,7 +88,7 @@ class contextmenu extends rcube_plugin
 
         // check if the skin dir is in the plugin folder or in the core skins
         // folder (external to this plugin) see RC #7445 for more info
-        if (strpos($rel_path, 'plugins/') !== false) {
+        if (str_contains($rel_path, 'plugins/')) {
             $base_path = slashify(RCUBE_INSTALL_PATH);
             $template_include_path = '.';
         }
